1720bb2181930bf2d973e3689070bff66b9c2c88
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2019-12-15 23:21:07 +0200

    updated rollup plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import { uglify } from "rollup-plugin-uglify";
+import uglify from "@lopatnov/rollup-plugin-uglify";
 
 import pkg from "./package.json";
 
